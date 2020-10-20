40b38257cdd706917589a4d855e0d97108e3b45d
Venkatesh <naiduvenkatesh802@gmail.com>
2020-10-20 17:29:45 +0530

    details screen and navigation drawer

---
+++ app/src/main/java/god/arti/app/Utils.kt
@@ -0,0 +1,43 @@
+package god.arti.app
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+
+fun<A : Activity> Activity.startNewActivity(activity: Class<A>){
+    Intent(this, activity).also {
+        it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(it)
+    }
+}
+
+fun View.visible(isVisible: Boolean){
+    visibility = if(isVisible) View.VISIBLE else View.GONE
+}
+
+fun View.enable(enabled: Boolean){
+    isEnabled = enabled
+    alpha = if(enabled) 1f else 0.5f
+}
+
+fun View.snackbar(message : String,action : (() -> Unit)? = null){
+    val snackbar = Snackbar.make(this,message,Snackbar.LENGTH_LONG)
+    action?.let {
+        snackbar.setAction("Retry"){
+            it()
+        }
+    }
+    snackbar.show()
+}
+
+fun Context.toast( message : String){
+    Toast.makeText(this, message , Toast.LENGTH_SHORT).show()
+}
+
+
+
+
--- app/src/main/java/god/arti/app/activity/HomeActivity.kt
+++ app/src/main/java/god/arti/app/activity/HomeActivity.kt
@@ -1,21 +1,78 @@
 package god.arti.app.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import god.arti.app.R
 import god.arti.app.databinding.ActivityHomeBinding
+import god.arti.app.toast
+import kotlinx.android.synthetic.main.activity_home.*
 
-class HomeActivity : AppCompatActivity() {
-    lateinit var activityHomeBinding: ActivityHomeBinding
-    lateinit var navController: NavController
+class HomeActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener {
+    lateinit var activityHomeBinding:ActivityHomeBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityHomeBinding=DataBindingUtil.setContentView(this,R.layout.activity_home)
+        setSupportActionBar(toolbar)
         navController = Navigation.findNavController(this, R.id.fragment_home)
+        setupDrawerLayout()
+        toolbar.setTitle("The Aarti App")
+    }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            navController,
+            drawer_layout)
+    }
+
+    private fun setupDrawerLayout() {
+        NavigationUI.setupWithNavController(nav_view,navController)
+        NavigationUI.setupActionBarWithNavController(this, navController,drawer_layout)
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+
+       when(menuItem.itemId){
+
+            R.id.aboutUsScreen ->{
+                toast("aboutUsScreen clicked")
+            }
+
+           R.id.recomendationScreen ->{
+               toast("recomendationScreen clicked")
+           }
+
+           R.id.businessScreen ->{
+               toast("businessScreen clicked")
+           }
+
+           R.id.rateUsScreen ->{
+               toast("rateUsScreen clicked")
+           }
+       }
+
+        menuItem.setChecked(true);
+        drawer_layout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
